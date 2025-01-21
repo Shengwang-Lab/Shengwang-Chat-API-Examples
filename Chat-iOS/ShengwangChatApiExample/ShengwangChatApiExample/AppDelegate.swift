70c60ae67e8e57b7e9c79d11516e2ff6db224a06
朱继超 <984065974@qq.com>
2025-01-21 14:09:09 +0800

    Update AppDelegate.swift

--- Chat-iOS/ShengwangChatApiExample/ShengwangChatApiExample/AppDelegate.swift
+++ Chat-iOS/ShengwangChatApiExample/ShengwangChatApiExample/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let options = AgoraChatOptions(appId: <#App Id#><##>)
+        let options = AgoraChatOptions(appId: <#AppId#>)
         options.enableConsoleLog = true
         AgoraChatClient.shared().initializeSDK(with: options)
         return true
