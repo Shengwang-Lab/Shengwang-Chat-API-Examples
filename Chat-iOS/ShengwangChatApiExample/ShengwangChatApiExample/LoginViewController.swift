790a049a7e8d170f9e5b20431561e7e1f2211f9d
朱继超 <984065974@qq.com>
2025-01-21 14:31:29 +0800

    Update LoginViewController.swift

--- Chat-iOS/ShengwangChatApiExample/ShengwangChatApiExample/LoginViewController.swift
+++ Chat-iOS/ShengwangChatApiExample/ShengwangChatApiExample/LoginViewController.swift
@@ -19,8 +19,6 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        loginButton.layer.cornerRadius = 4
-        loginButton.clipsToBounds = true
     }
 
 
