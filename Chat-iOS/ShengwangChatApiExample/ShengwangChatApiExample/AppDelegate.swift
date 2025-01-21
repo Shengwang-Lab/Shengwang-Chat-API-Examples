c03a0965b0eac6c9d053721c540eb5bd7f5196e2
朱继超 <984065974@qq.com>
2025-01-21 16:26:50 +0800

    disable auto login

--- Chat-iOS/ShengwangChatApiExample/ShengwangChatApiExample/AppDelegate.swift
+++ Chat-iOS/ShengwangChatApiExample/ShengwangChatApiExample/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         let options = AgoraChatOptions(appId: <#AppId#>)
         options.enableConsoleLog = true
+        options.isAutoLogin = false
         AgoraChatClient.shared().initializeSDK(with: options)
         return true
     }
